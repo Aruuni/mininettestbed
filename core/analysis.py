dedf4ef493b53f60fd66841b31063342d6aec805
Mihai <mihaifcp44279@gmail.com>
2024-06-10 21:39:34 +0100

    added more general support for system cc when parsing iperf output

--- core/analysis.py
+++ core/analysis.py
@@ -20,44 +20,8 @@ def process_raw_outputs(path):
         sender_ip = str(flow[2])
         receiver_ip = str(flow[3])
         start_time = int(flow[-3])
-        if flow[-2] == 'bbr':
-            port=5201
-
-            # Convert sender output into csv
-            df = parse_iperf_json(path+"/%s_output.txt" % sender, start_time)
-            df.to_csv("%s/%s.csv" % (csv_path,sender), index=False)
-
-            # Convert receiver output into csv
-            df = parse_iperf_json(path+"/%s_output.txt" % receiver, start_time)
-            df.to_csv("%s/%s.csv" % (csv_path, receiver), index=False)
-            probe_df = parse_tcp_probe_output(path + "/tcp_probe.txt", '%s:%s' % (receiver_ip, port), key='destination')
-            probe_df.to_csv("%s/%s_probe.csv" % (csv_path, sender),index=False)
-        if flow[-2] == 'bbr1':
-            port=5201
-
-            # Convert sender output into csv
-            df = parse_iperf_json(path+"/%s_output.txt" % sender, start_time)
-            df.to_csv("%s/%s.csv" % (csv_path,sender), index=False)
-
-            # Convert receiver output into csv
-            df = parse_iperf_json(path+"/%s_output.txt" % receiver, start_time)
-            df.to_csv("%s/%s.csv" % (csv_path, receiver), index=False)
-            probe_df = parse_tcp_probe_output(path + "/tcp_probe.txt", '%s:%s' % (receiver_ip, port), key='destination')
-            probe_df.to_csv("%s/%s_probe.csv" % (csv_path, sender),index=False)
-        if flow[-2] == 'cubic':
-            port=5201
-
-            # Convert sender output into csv
-            df = parse_iperf_json(path+"/%s_output.txt" % sender, start_time)
-            df.to_csv("%s/%s.csv" % (csv_path,sender), index=False)
-
-            # Convert receiver output into csv
-            df = parse_iperf_json(path+"/%s_output.txt" % receiver, start_time)
-            df.to_csv("%s/%s.csv" % (csv_path, receiver), index=False)
-            probe_df = parse_tcp_probe_output(path + "/tcp_probe.txt", '%s:%s' % (receiver_ip, port), key='destination')
-            probe_df.to_csv("%s/%s_probe.csv" % (csv_path, sender),index=False)
-
-        elif flow[-2] == 'orca':
+        #ADD An ADDITIONAL SPECIAL CASE IF NEEDED 
+        if flow[-2] == 'orca':
             port=4444
             # Convert sender output into csv
             df = parse_orca_output(path+"/%s_output.txt" % sender, start_time)
@@ -79,3 +43,16 @@ def process_raw_outputs(path):
             # Convert receiver output into csv
             df = parse_aurora_output(path+"/%s_output.txt" % receiver, start_time)
             df.to_csv("%s/%s.csv" %  (csv_path, receiver),index=False)
+        # WILL ONLY WORK WITH SYSTEM CC
+        else:
+            port=5201
+
+            # Convert sender output into csv
+            df = parse_iperf_json(path+"/%s_output.txt" % sender, start_time)
+            df.to_csv("%s/%s.csv" % (csv_path,sender), index=False)
+
+            # Convert receiver output into csv
+            df = parse_iperf_json(path+"/%s_output.txt" % receiver, start_time)
+            df.to_csv("%s/%s.csv" % (csv_path, receiver), index=False)
+            probe_df = parse_tcp_probe_output(path + "/tcp_probe.txt", '%s:%s' % (receiver_ip, port), key='destination')
+            probe_df.to_csv("%s/%s_probe.csv" % (csv_path, sender),index=False)
