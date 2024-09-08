66c09ca5ab59c0df7fc471e74ff762b476839e75
Mihai <mihaifcp44279@gmail.com>
2024-09-08 14:34:18 +0100

    added version detection

--- install.sh
+++ install.sh
@@ -1,6 +1,19 @@
-#installs python from source with make altinstall, caled with python3.X
-#sudo apt-get update && sudo apt-get install -y build-essential moreutils libssl-dev zlib1g-dev libncurses5-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev && wget https://www.python.org/ftp/python/3.7.12/Python-3.7.12.tgz && tar -xf Python-3.7.12.tgz && cd Python-3.7.12 && ./configure --enable-optimizations && make -j $(nproc) && sudo make altinstall
-sudo add-apt-repository -y ppa:jblgf0/python
-sudo apt update
-sudo apt install -y python3-pip openvswitch-testcontroller mininet moreutils sysstat ethtool iperf3
-sudo python3 -m pip install -y mininet numpy matplotlib pandas #--break-system-packages
+#!/bin/bash
+
+#TODO: add the orca/sage dependencies
+UBUNTU_VERSION=$(lsb_release -r | awk '{print $2}')
+
+if [[ "$UBUNTU_VERSION" == "16.04" ]]; then
+    # Execute if on Ubuntu 16.04
+    echo "Detected Ubuntu 16.04 installing python3.7"
+    sudo add-apt-repository -y ppa:jblgf0/python
+    sudo apt update
+    sudo apt install -y python3-pip openvswitch-testcontroller mininet python3.7 python3.7-dev moreutils sysstat ethtool iperf3
+    sudo python3.7 -m pip install pip==20.3.4
+    sudo python3.7 -m pip install  mininet numpy==1.18.5 matplotlib==3.1.3 pandas==1.0.5 tensorflow==1.14.0
+else
+    # Execute if on any other Ubuntu version
+    sudo apt update
+    sudo apt install -y python3-pip openvswitch-testcontroller mininet moreutils sysstat ethtool iperf3
+    sudo python3 -m pip install mininet numpy matplotlib pandas
+fi
