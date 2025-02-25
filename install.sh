f8ee89a12eda17d3808ca3c18b9282e65b6a0d7e
Aruuni <mihaifcp44279@gmail.com>
2025-02-25 00:00:44 +0000

    removed the older orca repo

--- install.sh
+++ install.sh
@@ -47,13 +47,13 @@ cd ../..
 bash build.sh
 
 
-echo "Downloading and setting up ns-3"
-git clone https://gitlab.com/nsnam/ns-3-dev ~/ns-3-dev
-cp ns3_simscript/CCTestBed.cc ~/ns-3-dev/scratch
-cp ns3_simscript/cross_path.cc ~/ns-3-dev/scratch
-cd ~/ns-3-dev
-./ns3 configure --build-profile=optimized 
-./ns3 
+# echo "Downloading and setting up ns-3"
+# git clone https://gitlab.com/nsnam/ns-3-dev ~/ns-3-dev
+# cp ns3_simscript/CCTestBed.cc ~/ns-3-dev/scratch
+# cp ns3_simscript/cross_path.cc ~/ns-3-dev/scratch
+# cd ~/ns-3-dev
+# ./ns3 configure --build-profile=optimized 
+# ./ns3 
 
 # TODO FINISH SAGE
 
