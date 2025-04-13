10a90d8a73027f5f8b868426ac60c137b5147fe5
Aruuni <mihaifcp44279@gmail.com>
2025-04-13 17:40:17 +0100

    updated install

--- install.sh
+++ install.sh
@@ -1,26 +1,9 @@
 #!/bin/bash
-CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-UBUNTU_VERSION=$(lsb_release -r | awk '{print $2}')
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt update
+sudo python3 -m pip install mininet numpy==2.1.3 matplotlib==3.9.2 pandas==2.2.3 scienceplots networkx scipy
 
-if [[ "$UBUNTU_VERSION" == "16.04" ]]; then
-    # Execute if on Ubuntu 16.04
-    echo "Detected Ubuntu 16.04 installing python3.7"
-    sudo add-apt-repository -y ppa:jblgf0/python
-    sudo apt update
-    sudo apt install -y python3-pip python3.7 python3.7-dev python3.6 python3.6-dev
-    sudo python3.7 -m pip install pip==20.3.4
-    sudo python3.7 -m pip install networkx scipy
-    sudo python3.7 -m pip install  mininet numpy==1.18.5 matplotlib==3.1.3 pandas==1.0.5 tensorflow==1.14.0
-    sudo python3.7 -m pip install --ignore-installed --upgrade pexpect
 
-else
-    # Execute if on any other Ubuntu version
-    sudo add-apt-repository ppa:deadsnakes/ppa
-    sudo apt update
-    sudo apt install -y python3-pip python3.7 python3.7-dev python3.7-distutils
-    sudo pip3 install -U virtualenv==15.2.*
-    sudo python3 -m pip install mininet numpy==2.1.3 matplotlib==3.9.2 pandas==2.2.3 scienceplots networkx scipy
-
-fi
 
 sudo apt install -y openvswitch-testcontroller mininet moreutils sysstat ethtool iperf3 cmake g++ g++-9 nlohmann-json3-dev software-properties-common
+sudo apt install -y wireshark
