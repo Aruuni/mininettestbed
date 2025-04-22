0f02769619efaf69d34b652d76ea4610cbdf44da
Aruuni <mihaifcp44279@gmail.com>
2025-04-22 16:24:52 +0100

    redirected the insmod output to devnull

--- setup.sh
+++ setup.sh
@@ -4,8 +4,8 @@ UBUNTU_VERSION=$(lsb_release -r | awk '{print $2}')
 
 # if you nee to use secure boot, you must sign theese, like this 
 # sudo /lib/modules/$(uname -r)/build/scripts/sign-file sha256 ~/bbr/mok.key ~/bbr/mok.pem CC/PCC-Kernel/src/tcp_pcc.ko
-sudo insmod CC/PCC-Kernel/src/tcp_pcc.ko
-sudo insmod CC/astraea-open-source/kernel/tcp-astraea/tcp_astraea.ko
+sudo insmod CC/PCC-Kernel/src/tcp_pcc.ko 2> /dev/null
+sudo insmod CC/astraea-open-source/kernel/tcp-astraea/tcp_astraea.ko 2> /dev/null
 
 #Orca settigns
 sudo sysctl -w net.ipv4.tcp_low_latency=1
