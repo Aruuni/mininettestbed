128d9d5423cf5539a3e9dcb90bd9b821444fdceb
Aruuni <mihaifcp44279@gmail.com>
2025-06-03 15:31:42 +0100

    experiments update

--- run_experiments.sh
+++ run_experiments.sh
@@ -3,7 +3,7 @@ source common.sh
 bash setup.sh
 
 # PROTOCOLS="sage"
-PROTOCOLS="orca"
+PROTOCOLS="astraea cubic bbr3 vivace-uspace"
 STEPS="10 20 30 40 50 60 70 80 90 100"
 halfSTEPS="5 10 15 20 25 30 35 40 45 50"
 bwSTEPS="150 200 250 300 350 400 450 500"
@@ -58,20 +58,20 @@ HOPS="3 5 6"
 #     done
 # done
 
-# CUBIC COEXISTANCE/BACKWARDS COMPATIBILITY WITH FLOWS
-for flows in $FLOWS_STEPS
-do
-    for qmult in $QMULTS
-    do
-        for protocol in $PROTOCOLS
-        do
-            for run in $RUNS
-            do
-              run experiments_mininet/friendly/experiment_cubic_flow_friendliness.py "15" "100" $qmult $protocol $run fifo 0 $flows
-            done
-        done
-    done
-done
+# # CUBIC COEXISTANCE/BACKWARDS COMPATIBILITY WITH FLOWS
+# for flows in $FLOWS_STEPS
+# do
+#     for qmult in $QMULTS
+#     do
+#         for protocol in $PROTOCOLS
+#         do
+#             for run in $RUNS
+#             do
+#               run experiments_mininet/friendly/experiment_cubic_flow_friendliness.py "15" "100" $qmult $protocol $run fifo 0 $flows
+#             done
+#         done
+#     done
+# done
 
 # # CUBIC COEXISTANCE/BACKWARDS COMPATIBILITY
 # for del in $STEPS
@@ -241,28 +241,47 @@ done
 #       Starlink_SD_Shanghai_15_ISL_path.log    88.811   740 pkts
 #
 # PROTOCOLS="satcp"
-# RUNS="1 2 3 4 5"
-# PATHS=( "Starlink_SD_NY_15_ISL_path.log" \
-#         "Starlink_SD_NY_15_BP_path.log" \
-#         "Starlink_SEA_NY_15_ISL_path.log" \
-#         "Starlink_SEA_NY_15_BP_path.log" \
-#         "Starlink_SD_SEA_15_ISL_path.log" \
-#         "Starlink_SD_SEA_15_BP_path.log" 
-#         "Starlink_NY_LDN_15_ISL_path.log" \
-#         "Starlink_SD_Shanghai_15_ISL_path.log" )
-# QUEUE_PKTS=(522 408 388 326 582 219 696 740)
 
-# for protocol in $PROTOCOLS; do
-#     for idx in "${!PATHS[@]}"; do
-#         path="${PATHS[$idx]}"
-#         pkts="${QUEUE_PKTS[$idx]}"
-#         for run in $RUNS; do
-#             run experiments_mininet/LeoEM/emulator.py "$path" [0] 100 "$pkts" "$protocol" "$run" 300
-#         done
-#     done
-# done
+PATHS=( "Starlink_SD_NY_15_ISL_path.log" \
+        "Starlink_SD_NY_15_BP_path.log" \
+        "Starlink_SEA_NY_15_ISL_path.log" \
+        "Starlink_SEA_NY_15_BP_path.log" \
+        "Starlink_SD_SEA_15_ISL_path.log" \
+        "Starlink_SD_SEA_15_BP_path.log" 
+        "Starlink_NY_LDN_15_ISL_path.log" \
+        "Starlink_SD_Shanghai_15_ISL_path.log" )
+QUEUE_PKTS=(522 408 388 326 582 219 696 740)
+# TESTING
+# PATHS=( "Starlink_SEA_BsAs_15_ISL_path.log" )
+# QUEUE_PKTS=(9999)
 
+for qmult in $QMULTS; do
+    for protocol in $PROTOCOLS; do
+        for idx in "${!PATHS[@]}"; do
+            path="${PATHS[$idx]}"
+            base_pkts="${QUEUE_PKTS[$idx]}"
+            adj_pkts=$(awk "BEGIN {printf \"%d\", $base_pkts * $qmult}")
+            for run in $RUNS; do
+                run experiments_mininet/LeoEM/emulator.py \
+                    "$path" "[0]" 100 "$adj_pkts" "$protocol" "$run" 300
+            done
+        done
+    done
+done
 
+for qmult in $QMULTS; do
+    for protocol in $PROTOCOLS; do
+        for idx in "${!PATHS[@]}"; do
+            path="${PATHS[$idx]}"
+            base_pkts="${QUEUE_PKTS[$idx]}"
+            adj_pkts=$(awk "BEGIN {printf \"%d\", $base_pkts * $qmult}")
+            for run in $RUNS; do
+                run experiments_mininet/LeoEM/emulator.py \
+                    "$path" "[0, 100]" 100 "$adj_pkts" "$protocol" "$run" 300
+            done
+        done
+    done
+done
 # PROTOCOLS="satcpbbr1"
 # RUNS="6"
 # PATHS=(  "Starlink_SD_SEA_15_ISL_path.log" )
