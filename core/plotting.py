b5ee9e72ac3d9b03d639504458e9ec5aaffb41fa
Aruuni <mihaifcp44279@gmail.com>
2025-06-20 10:19:11 +0100

    prolly shouldnt let this in

--- core/plotting.py
+++ core/plotting.py
@@ -4,8 +4,8 @@ ELINEWIDTH = 0.75
 CAPTHICK = ELINEWIDTH
 CAPSIZE= 2
 PROTOCOLS_FRIENDLY_NAME_EXTENSION = {'cubic': 'Cubic', 'orca': 'Orca', 'bbr3': 'BBRv3', 'sage': 'Sage', 'vivace': 'Vivace', 'astraea': 'Astraea', 'vivace-uspace': 'Vivace', 'bbr1': 'BBRv1', 'astraea_old': 'Astraea (old)', 'orca-100': 'Orca-100msMTP'}
-#PROTOCOLS_EXTENSION = ['cubic', 'sage', 'orca', 'astraea', 'bbr3', 'vivace-uspace']
-PROTOCOLS_EXTENSION = [ 'orca', 'orca-100']
+PROTOCOLS_EXTENSION = ['cubic', 'sage', 'orca', 'astraea', 'bbr3', 'vivace-uspace']
+
 PROTOCOLS_MARKERS_EXTENSION = {'cubic': 'x', 'orca': '+', 'bbr3': '.', 'sage': '*', 'astraea_old': '4', 'astraea': '2', 'vivace-uspace': '_', 'bbr1': '1', 'orca-100': '3'}
 COLORS_EXTENSION = {'cubic': '#0C5DA5', 'orca': '#00B945', 'bbr3': '#FF9500', 'sage': '#FF2C01', 'astraea_old': '#845B97', 'astraea': '#686868', 'vivace-uspace': '#845B97', 'bbr1': '#964B00', 'orca-100': "#056428"}
 
