3122dfd45736e4ee2ac5998725aa0562db19d4f2
Aruuni <mihaifcp44279@gmail.com>
2025-08-11 21:39:57 +0100

    Remove deleted files

--- common.sh
+++
@@ -1,9 +0,0 @@
-run() {
-    UBUNTU_VERSION=$(lsb_release -r | awk '{print $2}')
-
-    if [[ "$UBUNTU_VERSION" == "16.04" ]]; then
-        time python3.7 "$@"
-    else
-        time python3 "$@"
-    fi
-}
