b229dcf09602ff8e27b15ffcbaa307d2c6216f04
Mihai <mihaifcp44279@gmail.com>
2025-08-17 16:25:51 +0100

    Update plot.py

--- aggregate_plots/figure_cross_path_scatter/plot.py
+++ aggregate_plots/figure_cross_path_scatter/plot.py
@@ -13,7 +13,7 @@ from matplotlib.patches import Ellipse
 import matplotlib.transforms as transforms
 
 script_dir = os.path.dirname(__file__)
-mymodule_dir = os.path.join(script_dir, '../../..')
+mymodule_dir = os.path.join(script_dir, '../..')
 sys.path.append(mymodule_dir)
 from core.config import *
 from core.plotting import *
